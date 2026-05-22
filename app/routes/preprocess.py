64654dc16681fe53315af77ecc5edafadbfd9df4
PandaL33 <lghut@outlook.com>
2026-05-22 13:19:41 +0800

    增加点云配准

--- app/routes/preprocess.py
+++ app/routes/preprocess.py
@@ -11,12 +11,14 @@ import open3d as o3d
 import uuid
 import os
 from pathlib import Path
-
+from app.services.cloud_compare_icp import CloudCompareIcp
 router = APIRouter()
-
+import logging
+logger = logging.getLogger(__name__)
 @router.post("/preprocess")
 async def preprocess_endpoint(
     file_url: str = Form(...),
+    map_file_url: str = Form(None),  # 修改：将map_file_url设为可选参数
     background_tasks: BackgroundTasks = None
 ):
     temp_input = None
@@ -26,12 +28,25 @@ async def preprocess_endpoint(
         pcd = o3d.io.read_point_cloud(temp_input)
         if pcd.is_empty():
             raise ValueError("Empty point cloud")
-
+        
+        uuid_str = str(uuid.uuid4())
+        
+        if map_file_url:
+            map_input = download_pcd_to_temp(map_file_url, settings.max_file_size)
+            map_pcd = o3d.io.read_point_cloud(map_input)
+            logger.info(f"点云配准,align_cloud:{temp_input},map_cloud:{map_input}")
+            if not map_pcd.is_empty():
+                cloud_compare_icp = CloudCompareIcp()
+                icp_pcd_file_path = settings.preprocessed_dir / f"{uuid_str}_icp.pcd"
+                transform_matrix, fitness, iterations, icp_pcd = cloud_compare_icp.run_registrator(temp_input, map_input, icp_pcd_file_path,0.25)
+                pcd = icp_pcd
+        
         # 预处理
+        logger.info(f"点云预处理开始")
         preprocessor = PointCloudPreprocessor()
         cleaned = preprocessor.preprocess(pcd)
 
-        output_path = settings.preprocessed_dir / f"{uuid.uuid4()}.pcd"
+        output_path = settings.preprocessed_dir / f"{uuid_str}.pcd"
         if not o3d.io.write_point_cloud(str(output_path), cleaned):
             raise RuntimeError("Save failed")
 
